edd5636a72b24ff2291a52d4fce8fb5ad320bdf8
lmpbarga <lucasmpbarga@gmail.com>
2019-03-11 00:28:46 -0300

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Information Person Service
                 </div>
 
                 <div class="links">
